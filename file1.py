0e05b9754e9ac6163a4dad7d4cb7f3e0f58e84e1
foreverbala <120767488+foreverbala@users.noreply.github.com>
2022-12-17 04:47:39 -0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from file2 import *
+
+def user_input():
+ arr = []
+ given_input = ''
+
+ while len(arr) < 6:
+    try:
+       given_input = float(input('Enter an number: '))
+       arr.append(given_input)
+    except ValueError :
+        print('please enter a number')
+        continue
+ return arr 
+user_input()   
+if err_fn() is True:
+    user_input()        
+
